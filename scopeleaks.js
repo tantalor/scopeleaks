907887e12e9e7a6b61a735553c5ac68d010d64af
John Tantalo <john.tantalo@gmail.com>
2011-01-13 21:22:19 -0800

    factored out methods object

--- scopeleaks.js
+++ scopeleaks.js
@@ -13,26 +13,24 @@
 		return snapshot;
 	};
 	
-	var methods = {
-		leaks: function() {
-			var ss = snapshot();
-			var leaks = [];
+	function leaks() {
+		var ss = snapshot();
+		var leaks = [];
 
-			for (var i in ss)
-				if (!(i in methods) &&
-					!(scope.document && scope.document.getElementById(ss[i]) != null) &&
-					!(typeof(scope.opera) == 'object' && scope.opera.toString() == "[object Opera]" && ss[i] == "onhashchange") &&
-					original.indexOf(ss[i]) == -1)
-					leaks.push(ss[i]);
-			
-			return leaks;
-		}
+		for (var i in ss)
+			if (
+				!(scope.document && scope.document.getElementById(ss[i]) != null) &&
+				!(typeof(scope.opera) == 'object' && scope.opera.toString() == "[object Opera]" && ss[i] == "onhashchange") &&
+				original.indexOf(ss[i]) == -1)
+				leaks.push(ss[i]);
+		
+		return leaks;
 	};
 	
   if (typeof(window) !== 'undefined') {
-    window.scopeleaks = methods;
+    window.scopeleaks = {leaks: leaks};
   } else if (typeof(exports) !== 'undefined') {
-    exports.leaks = methods.leaks;
+    exports.leaks = leaks;
   }
   
 	snapshot();
